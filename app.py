7f3885dcd95774ac81fefbccdf192af6d2f455ab
Pandemonium221 <kostantinos.tasioulas@gmail.com>
2022-10-09 15:28:38 +0300

    ASHDASHBD

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Hello, welcome to Pandemonium Brothel")
+name = input("Pws se lene?\n")
+if name == "Porth":
+    print("Porth pare ton mpoulo, tha meineis agamhtos mia zwh")
+    exit()
+else:
+    print("Hello " + name + ", kalwsorisate sto tapeino moy mpoyrdelo.")
+#porti nothing personal kid
+gynaika = "Latines,Asiatises,Ebony,Teen,Milf,Gilf"
+print(name +", ti tha thelate apo tis shmerines mas poytanes? Shmera exoume diathesimes.\n" + gynaika)
+paraggelia = input()
+plhthos= input("Poses poutanes tha thelate?\n")
+kostos = 30
+synolika = kostos * int(plhthos)
+print("Eyxaristw poly. To synoliko kostos einai: $" + str(synolika))
+print("Teleia " + name + ",tha exoume ta poutanakia etoima gia sena se 2 lepta.")
+
+
+
+
+
+
